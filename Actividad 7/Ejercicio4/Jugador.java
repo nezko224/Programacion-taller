d0bc6b9a5f3f8887042c94192053a3f83e8fed04
nezko224 <sasha.cabrera.1ro3ma@gmail.com>
2026-06-08 11:26:20 -0300

    Refactor Jugador class structure

--- Actividad 7/Ejercicio4/Jugador.java
+++ Actividad 7/Ejercicio4/Jugador.java
@@ -1,28 +1,27 @@
 package Ejercicio4;
 
 public class Jugador {
-    private String  nombre;
-    private String  posicion;
-    private int     edad;
-    private String  clubActual;
+    private String nombre;
+    private String posicion;
+    private int edad;
+    private String clubActual;
     private boolean convocado;
 
     public Jugador(String nombre, String posicion, int edad, String clubActual, boolean convocado) {
-        this.nombre     = nombre;
-        this.posicion   = posicion;
-        this.edad       = edad;
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.edad = edad;
         this.clubActual = clubActual;
-        this.convocado  = convocado;
+        this.convocado = convocado;
     }
 
-    public String  getNombre()   { return nombre; }
-    public String  getPosicion() { return posicion; }
-    public int     getEdad()     { return edad; }
-    public String  getClub()     { return clubActual; }
+    public String getNombre() { return nombre; }
+    public String getPosicion() { return posicion; }
+    public int getEdad() { return edad; }
+    public String getClub() { return clubActual; }
     public boolean isConvocado() { return convocado; }
-    public void    setConvocado(boolean c) { this.convocado = c; }
+    public void setConvocado(boolean c) { this.convocado = c; }
 
-    @Override
     public String toString() {
         return nombre + " | " + posicion + " | Edad: " + edad
             + " | Club: " + clubActual
