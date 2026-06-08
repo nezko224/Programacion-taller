cc0ad2b7e52bdafd0a32c2e37d173837b9af7714
nezko224 <sasha.cabrera.1ro3ma@gmail.com>
2026-06-08 11:27:36 -0300

    Update Main.java

--- Actividad 7/Ejercicio4/Main.java
+++ Actividad 7/Ejercicio4/Main.java
@@ -4,29 +4,29 @@ public class Main {
 
         String[][] datos = {
             // Argentina
-            {"Emiliano Martinez",   "Arquero",       "33", "Aston Villa",     "true"},
-            {"Cristian Romero",     "Defensor",      "28", "Tottenham",       "true"},
-            {"Nicolas Otamendi",    "Defensor",      "38", "Benfica",         "true"},
-            {"Nahuel Molina",       "Defensor",      "28", "Atletico Madrid", "true"},
-            {"Enzo Fernandez",      "Mediocampista", "25", "Chelsea",         "true"},
-            {"Rodrigo De Paul",     "Mediocampista", "32", "Inter Miami",     "true"},
-            {"Alexis Mac Allister", "Mediocampista", "27", "Liverpool",       "true"},
-            {"Lionel Messi",        "Delantero",     "39", "Inter Miami",     "true"},
-            {"Julian Alvarez",      "Delantero",     "26", "Atletico Madrid", "true"},
-            {"Lautaro Martinez",    "Delantero",     "29", "Inter Milan",     "true"},
-            {"Paulo Dybala",        "Delantero",     "33", "Roma",            "false"},
+            {"Emiliano Martinez", "Arquero", "33", "Aston Villa", "true"},
+            {"Cristian Romero", "Defensor", "28", "Tottenham", "true"},
+            {"Nicolas Otamendi", "Defensor", "38", "Benfica", "true"},
+            {"Nahuel Molina", "Defensor", "28", "Atletico Madrid", "true"},
+            {"Enzo Fernandez", "Mediocampista", "25", "Chelsea", "true"},
+            {"Rodrigo De Paul", "Mediocampista", "32", "Inter Miami", "true"},
+            {"Alexis Mac Allister", "Mediocampista", "27", "Liverpool", "true"},
+            {"Lionel Messi", "Delantero", "39", "Inter Miami", "true"},
+            {"Julian Alvarez", "Delantero", "26", "Atletico Madrid", "true"},
+            {"Lautaro Martinez", "Delantero", "29", "Inter Milan", "true"},
+            {"Paulo Dybala", "Delantero", "33", "Roma", "false"},
             // Francia
-            {"Mike Maignan",        "Arquero",       "31", "Milan",           "true"},
-            {"William Saliba",      "Defensor",      "25", "Arsenal",         "true"},
-            {"Theo Hernandez",      "Defensor",      "29", "Milan",           "true"},
-            {"Jules Kounde",        "Defensor",      "28", "Barcelona",       "true"},
-            {"Aurelien Tchouameni","Mediocampista",  "26", "Real Madrid",     "true"},
-            {"Eduardo Camavinga",   "Mediocampista", "24", "Real Madrid",     "true"},
-            {"Adrien Rabiot",       "Mediocampista", "31", "Marseille",       "true"},
-            {"Kylian Mbappe",       "Delantero",     "28", "Real Madrid",     "true"},
-            {"Ousmane Dembele",     "Delantero",     "29", "PSG",             "true"},
-            {"Marcus Thuram",       "Delantero",     "29", "Inter Milan",     "true"},
-            {"Antoine Griezmann",   "Delantero",     "35", "Atletico Madrid", "false"}
+            {"Mike Maignan", "Arquero", "31", "Milan", "true"},
+            {"William Saliba", "Defensor", "25", "Arsenal", "true"},
+            {"Theo Hernandez", "Defensor", "29", "Milan", "true"},
+            {"Jules Kounde", "Defensor", "28", "Barcelona", "true"},
+            {"Aurelien Tchouameni", "Mediocampista", "26", "Real Madrid", "true"},
+            {"Eduardo Camavinga", "Mediocampista", "24", "Real Madrid", "true"},
+            {"Adrien Rabiot", "Mediocampista", "31", "Marseille", "true"},
+            {"Kylian Mbappe", "Delantero", "28", "Real Madrid", "true"},
+            {"Ousmane Dembele", "Delantero", "29", "PSG", "true"},
+            {"Marcus Thuram", "Delantero", "29", "Inter Milan", "true"},
+            {"Antoine Griezmann", "Delantero", "35", "Atletico Madrid", "false"}
         };
 
         Jugador[] jugadores = new Jugador[datos.length];
