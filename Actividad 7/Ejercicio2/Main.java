70efe4c6dea693a51899a1320d2c0e39440af6b6
nezko224 <sasha.cabrera.1ro3ma@gmail.com>
2026-06-08 11:22:22 -0300

    Update Main.java

--- Actividad 7/Ejercicio2/Main.java
+++ Actividad 7/Ejercicio2/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         String[][] datos = {
-            {"El Principito",                        "9789504000044", "3200"},
-            {"Donde viven los monstruos",            "9789877381501", "2800"},
-            {"Matilda",                              "9788420432885", "4500"},
-            {"Charlie y la fábrica de chocolate",    "9788420432878", "4500"},
-            {"El árbol generoso",                    "9789877380125", "2600"},
-            {"Harriet, la espía",                    "9788420401164", "5100"},
-            {"El ratón que se comió la luna",        "9788416648931", "2200"},
-            {"Las aventuras de Pippi Mediaslargas",  "9788420400891", "3900"},
-            {"El mago de Oz",                        "9789504000815", "3500"},
-            {"El hobbit",                            "9788445000687", "6200"}
+            {"El Principito","9789504000044","3200"},
+            {"Donde viven los monstruos","9789877381501","2800"},
+            {"Matilda","9788420432885","4500"},
+            {"Charlie y la fábrica de chocolate","9788420432878","4500"},
+            {"El árbol generoso","9789877380125","2600"},
+            {"Harriet, la espía","9788420401164","5100"},
+            {"El ratón que se comió la luna","9788416648931","2200"},
+            {"Las aventuras de Pippi Mediaslargas","9788420400891","3900"},
+            {"El mago de Oz","9789504000815","3500"},
+            {"El hobbit","9788445000687","6200"}
         };
 
         Libro[] libros = new Libro[datos.length];
