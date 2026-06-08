b3bf74b82eff2ba5e9b7144647cd3b460743234d
nezko224 <sasha.cabrera.1ro3ma@gmail.com>
2026-06-08 11:25:21 -0300

    Update Paciente.java

--- Actividad 7/Ejercicio3/Paciente.java
+++ Actividad 7/Ejercicio3/Paciente.java
@@ -1,24 +1,24 @@
 package Ejercicio3;
 
 public class Paciente {
-    private String  nombre;
-    private String  dni;
-    private int     edad;
-    private double  peso;
+    private String nombre;
+    private String dni;
+    private int edad;
+    private double peso;
     private boolean estaHospitalizado;
 
     public Paciente(String nombre, String dni, int edad, double peso, boolean estaHospitalizado) {
-        this.nombre            = nombre;
-        this.dni               = dni;
-        this.edad              = edad;
-        this.peso              = peso;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.peso = peso;
         this.estaHospitalizado = estaHospitalizado;
     }
 
-    public String  getNombre()           { return nombre; }
-    public String  getDni()              { return dni; }
-    public int     getEdad()             { return edad; }
-    public double  getPeso()             { return peso; }
+    public String getNombre() { return nombre; }
+    public String getDni() { return dni; }
+    public int getEdad() { return edad; }
+    public double getPeso() { return peso; }
     public boolean isEstaHospitalizado() { return estaHospitalizado; }
 
     @Override
